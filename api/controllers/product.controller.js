997c4357027074e3b4ff804378be82b4cc6da2db
Quynh <osam.quynh@gmail.com>
2020-02-21 15:24:51 +0700

    Add connect mongodb

---
+++ api/controllers/product.controller.js
@@ -0,0 +1,11 @@
+const Product = require('../../models/product.model')
+
+module.exports.index = async function(req, res) {
+    const products = await Product.find()
+    res.json(products)
+}
+
+module.exports.create = async function(req, res) {
+    const product =  await Product.create(req.body)
+    res.json(product)
+}
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+require('dotenv').config()
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParse = require('cookie-parser')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
 
 const userRoute = require('./routes/user.router')
 const authRoute = require('./routes/auth.router')
+
+const apiProductRoute = require('./api/routes/product.route')
 
 const authMiddleware = require('./middlewares/auth.middleware')
 
@@ -24,5 +31,6 @@ app.get('/', (req, res) => res.render('index'))
 
 app.use('/users', authMiddleware.requireAuth, userRoute)
 app.use('/auth', authRoute)
+app.use('/api/products', apiProductRoute)
 
 app.listen(port, () => console.log(`Running port ${port}`))
